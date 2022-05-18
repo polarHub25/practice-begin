2af768ac2f747f24040f40837019da22430f0f30
polarHub25 <olivetree258@gmail.com>
2022-05-18 19:43:37 +0900

    JPA Auditing 추가 작업 / 테스트 코드 작업 : BaseTimeEntity만 상속받으면 등록일 , 수정일 자동 저장 가능

--- src/main/java/com/practice/begin/springboot/domain/posts/Posts.java
+++ src/main/java/com/practice/begin/springboot/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.practice.begin.springboot.domain.posts;
 
+import com.practice.begin.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,7 @@ import javax.persistence.*;
 @Getter // 롬복의 어노테이션
 @NoArgsConstructor // 롬복의 어노테이션 : 기본 생성자 자동 추가 public Posts(){}
 @Entity // JPA 어노테이션 : 테이블과 링크될 클래스임을 나타냄
-public class Posts { //Entity 클래스
+public class Posts extends BaseTimeEntity { //Entity 클래스
 
     @Id // 해당 테이블의 pk , pk는 long 타입의 auto_increment 추천
     @GeneratedValue(strategy = GenerationType.IDENTITY) // pk의 생성 규칙
--- src/test/java/com/practice/begin/springboot/domain/PostsRepositoryTest.java
+++ src/test/java/com/practice/begin/springboot/domain/PostsRepositoryTest.java
@@ -1,5 +1,7 @@
-package com.practice.begin.springboot.domain.posts;
+package com.practice.begin.springboot.domain;
 
+import com.practice.begin.springboot.domain.posts.Posts;
+import com.practice.begin.springboot.domain.posts.PostsRepository;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -41,5 +44,28 @@ public class PostsRepositoryTest {
         Posts posts = postList.get(0);
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void BastTimeEntity_등록(){
+        //given
+        LocalDateTime now = LocalDateTime.of(2021,5,18,0,0,0);
+        postRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        //when
+        List<Posts> postList = postRepository.findAll();
+
+        //then
+        Posts posts = postList.get(0);
+
+        System.out.println(">>>>>>>>>>> createDate="+posts.getCreatedDate()+", modifiedDate ="+posts.getModifiedDate());
+
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
+
     }
 }
